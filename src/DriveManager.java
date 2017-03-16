1bb9b6558649dc56c960a9e679038c8a3c14b482
GABS <galbie_elinour_24rpl@student.smktelkom-mlg.sch.id>
2017-03-16 19:37:50 +0700

    satu

---
+++ src/DriveManager.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author GABS
+ */
+class DriveManager {
+
+    static Connection getConnection(String jdbcmysqllocalhost3306db_testkoneksizeroD, String root, String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
